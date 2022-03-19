243886f4504077a1a962507ce05f8bdad218b07b
hadinayebi <hadinayebi@gmail.com>
2022-03-19 13:50:02 -0400

    initial commit, home component

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'proteinlatentspace',
+    appId: '1:561472922982:web:ec5226f91ff494f39514a7',
+    storageBucket: 'proteinlatentspace.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDfsra4sXrinb8gYmgEgW-z7e8FuEaOWw0',
+    authDomain: 'proteinlatentspace.firebaseapp.com',
+    messagingSenderId: '561472922982',
+    measurementId: 'G-PE4FG0HMED',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'proteinlatentspace',
+    appId: '1:561472922982:web:ec5226f91ff494f39514a7',
+    storageBucket: 'proteinlatentspace.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDfsra4sXrinb8gYmgEgW-z7e8FuEaOWw0',
+    authDomain: 'proteinlatentspace.firebaseapp.com',
+    messagingSenderId: '561472922982',
+    measurementId: 'G-PE4FG0HMED',
+  },
   production: false
 };
 
